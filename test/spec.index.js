03ae687789a2885c12143e526612086d4431f374
Seb Insua <me@sebinsua.com>
2015-02-26 15:49:59 +0000

    Support for a date field which is configured as inexact with an inexact property on the field options. Defaults to exact.

--- test/spec.index.js
+++ test/spec.index.js
@@ -71,6 +71,150 @@ describe('Template Mixins', function () {
 
     });
 
+    describe('input-date', function () {
+
+        beforeEach(function () {
+            middleware = mixins(translate, {});
+        });
+
+        it('adds a function to res.locals', function () {
+            middleware(req, res, next);
+            res.locals['input-date'].should.be.a('function');
+        });
+
+        it('returns a function', function () {
+            middleware(req, res, next);
+            res.locals['input-date']().should.be.a('function');
+        });
+
+        it('renders thrice if the field is not marked as inexact', function () {
+            middleware(req, res, next);
+            res.locals['input-date']().call(res.locals, 'field-name');
+            render.should.have.been.calledThrice;
+        });
+
+        it('renders twice if the field is marked as inexact', function () {
+            var middlewareWithFieldNameMarkedAsInexact = mixins(translate, {
+                'field-name': {
+                    'inexact': true
+                }
+            });
+            middlewareWithFieldNameMarkedAsInexact(req, res, next);
+            res.locals['input-date']().call(res.locals, 'field-name');
+            render.should.have.been.calledTwice;
+        });
+
+        it('looks up field label', function () {
+            middleware(req, res, next);
+            res.locals['input-date']().call(res.locals, 'field-name');
+
+            render.called;
+
+            var dayCall = render.getCall(0),
+                monthCall = render.getCall(1),
+                yearCall = render.getCall(2);
+
+            dayCall.calledWith(sinon.match({
+              'class': 'date-input',
+              error: undefined,
+              hint: null,
+              id: 'field-name-day',
+              label: 'fields.field-name-day.label',
+              max: 31,
+              maxlength: 2,
+              min: 1,
+              pattern: '[0-9]*',
+              required: true,
+              type: 'text',
+              value: undefined
+            })).should.be.true;
+
+            monthCall.calledWith(sinon.match({
+              pattern: '[0-9]*',
+              min: 1,
+              max: 12,
+              maxlength: 2,
+              'class': 'date-input',
+              id: 'field-name-month',
+              type: 'text',
+              value: undefined,
+              label: 'fields.field-name-month.label',
+              hint: null,
+              error: undefined,
+              required: true
+            })).should.be.true;
+
+            yearCall.calledWith(sinon.match({
+              pattern: '[0-9]*',
+              maxlength: 4,
+              'class': 'date-input',
+              id: 'field-name-year',
+              type: 'text',
+              value: undefined,
+              label: 'fields.field-name-year.label',
+              hint: null,
+              error: undefined,
+              required: true
+            })).should.be.true;
+        });
+
+        it('prefixes translation lookup with namespace if provided', function () {
+            middleware = mixins(translate, {}, { sharedTranslationsKey: 'name.space' });
+            middleware(req, res, next);
+            res.locals['input-date']().call(res.locals, 'field-name');
+
+            render.called;
+
+            var dayCall = render.getCall(0),
+                monthCall = render.getCall(1),
+                yearCall = render.getCall(2);
+
+            dayCall.calledWith(sinon.match({
+              'class': 'date-input',
+              error: undefined,
+              hint: null,
+              id: 'field-name-day',
+              label: 'name.space.fields.field-name-day.label',
+              max: 31,
+              maxlength: 2,
+              min: 1,
+              pattern: '[0-9]*',
+              required: true,
+              type: 'text',
+              value: undefined
+            })).should.be.true;
+
+            monthCall.calledWith(sinon.match({
+              pattern: '[0-9]*',
+              min: 1,
+              max: 12,
+              maxlength: 2,
+              'class': 'date-input',
+              id: 'field-name-month',
+              type: 'text',
+              value: undefined,
+              label: 'name.space.fields.field-name-month.label',
+              hint: null,
+              error: undefined,
+              required: true
+            })).should.be.true;
+
+            yearCall.calledWith(sinon.match({
+              pattern: '[0-9]*',
+              maxlength: 4,
+              'class': 'date-input',
+              id: 'field-name-year',
+              type: 'text',
+              value: undefined,
+              label: 'name.space.fields.field-name-year.label',
+              hint: null,
+              error: undefined,
+              required: true
+            })).should.be.true;
+        });
+
+    });
+
     describe('input-submit', function () {
 
         beforeEach(function () {
