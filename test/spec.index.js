15e25c401a21603bdcd4e4fb3d79d3052abaeccb
Seb Insua <me@sebinsua.com>
2015-04-20 10:18:52 +0100

    Changed reserved name 'class' to 'className'.

--- test/spec.index.js
+++ test/spec.index.js
@@ -77,13 +77,13 @@ describe('Template Mixins', function () {
             it('should have classes if one or more were specified against the field', function () {
                 middleware = mixins(translate, {
                     'field-name': {
-                        'class': ['abc', 'def']
+                        'className': ['abc', 'def']
                     }
                 });
                 middleware(req, res, next);
                 res.locals['input-text']().call(res.locals, 'field-name');
                 render.should.have.been.calledWith(sinon.match({
-                    class: 'abc def'
+                    className: 'abc def'
                 }));
             });
 
