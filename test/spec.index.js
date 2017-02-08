e59f6ab8d8d8ec365e35b01d7e99ff3446681146
joefitter <hello@joefitter.com>
2017-02-08 11:47:40 +0000

    Feature - renderField
    Moved renderField mixin from hof-controllers. Added docs. This mixin when called with a field as a scope, will render a template mixin depending on the `mixin` property of the field.
    
    ```html
    {{#fields}}
        {{#renderField}}{{/renderField}}
    {{/fields}}
    ```
    
    ```js
    fields: [{
        key: 'my-field',
        mixin: 'input-text'
    }]
    ```

--- test/spec.index.js
+++ test/spec.index.js
@@ -1543,6 +1543,32 @@ describe('Template Mixins', function () {
 
         });
 
+        describe('renderField', function () {
+
+            var inputTextStub;
+
+            beforeEach(function () {
+                middleware(req, res, next);
+                inputTextStub = sinon.stub();
+                sinon.stub(res.locals, 'input-text').returns(inputTextStub);
+            });
+
+            afterEach(function () {
+                res.locals['input-text'].restore();
+            });
+
+            it('looks up a mixin from res.locals and calls it', function () {
+                var field = {
+                    key: 'my-field',
+                    mixin: 'input-text'
+                };
+                res.locals.renderField().call(field);
+                inputTextStub.should.have.been.calledOnce
+                    .and.calledWith('my-field');
+            });
+
+        });
+
         describe('Multiple lambdas', function () {
 
             it('recursively runs lambdas wrapped in other lambdas correctly', function () {
